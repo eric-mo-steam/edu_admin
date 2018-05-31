6c7459b4a4cac8c6429aeb81bbcbeedd9f1bb0bb
eric <mo_xiaojiang@163.com>
2018-05-31 09:33:26 +0800

    提交工程

---
+++ main.js
@@ -0,0 +1,30 @@
+var util = require('util')
+var url = require('url')
+var express = require('express')
+
+var app = express()
+
+// public下可放置图片资源文件
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/*.html', function (req, res) {
+    res.sendFile(__dirname + "/view/" + req.path)
+})
+
+
+app.get('/*', function (req, res) {
+    console.log("data:" + req.path)
+    res.writeHead(200, {'Content-Type': 'text/json'})
+    var response = {
+      "first_name":'eric',
+      "last_name":'mo'
+    };
+    res.end(JSON.stringify(response));
+})
+ 
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
