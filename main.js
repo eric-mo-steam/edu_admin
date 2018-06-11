f0210fa5dae7b3756f92b479ec966f381364b913
eric <mo_xiaojiang@163.com>
2018-06-11 15:10:33 +0800

    学生页面

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ app.use(express.static(__dirname + '/public'))
 
 // 路由转发，页面请求
 app.get('/*.html', function (req, res) {
-    res.sendFile(__dirname + "/view/" + req.path)
+    res.sendFile(__dirname + "/view/" + req.path, function(err) {
+        if (err) {
+            urlNotFound(req, res);
+        }
+    })
 })
 
 // 数据请求
@@ -26,11 +30,15 @@ app.get('/course/*', function (req, res) {
     var service = require('./service/course')
     service.course(req, res)
 })
+
 // 其他请求，返回404
 app.get('/*', function (req, res) {
-    console.log("404: " + req.path)
-    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'})
-    res.end()
+    urlNotFound(req, res);
+})
+
+// 其他的POST表单请求，返回500
+app.post('/*', function (req, res) {
+    badRequest(req, res);
 })
  
 var server = app.listen(8000, function () {
@@ -38,3 +46,15 @@ var server = app.listen(8000, function () {
     var port = server.address().port
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 })
+
+function badRequest(req, res) {
+    console.log("500: " + req.path)
+    res.writeHead(500, {'Content-Type': 'text/html;charset=utf-8'})
+    res.end()
+}
+
+function urlNotFound(req, res) {
+    console.log("404: " + req.path)
+    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'})
+    res.end()
+}
