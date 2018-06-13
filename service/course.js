2717800e241cd41d54549b05f435ddae7ff626df
unknown <a1254278911@gmail.com>
2018-06-13 11:05:14 +0800

    返回课程

--- service/course.js
+++ service/course.js
@@ -1,11 +1,20 @@
 // 数据库mysql连接器
 var conn = require('../database/connector')
-//返回课程的全部信息
-exports.course = function(req,res){
+//返回学生的未选的课程的全部信息
+exports.scourse = function(req,res,cookies){
     res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'})
-   var sql = 'select distinct * from course;'
-  
-    conn.query(sql, function(data){
-        res.end(JSON.stringify(data))
-    })
+   if (cookies) {
+            var sql = "select course.id,course.name,course.credit,teacher.name \
+                from course,teacher where course.tid=teacher.id and course.id not in(\
+                select cid from SC where sid = '"cookies.id"');"
+                conn.query(sql, function(data){
+                     console.log(data)
+                    res.end(JSON.stringify(data))
+                })
+    } 
+    else {
+        // 没有指定的cookie
+        var data = {errorCode:1,
+        errorMassage:"没有指定的cookie"}
+    }
 }
